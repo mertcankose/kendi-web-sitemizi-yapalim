174bdbab4dc937e6d2767e2db6edf27b1fbae23c
mertcankose <mertcankose97@outlook.com>
2021-05-28 18:09:58 +0300

    video-4

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/global.css'
 import Header from '../components/header'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="antialiased text-gray-800">
-      <Header />
-      <main className="mt-6 mb-10">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain="mertcan-kose-blog.us.auth0.com"
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className="antialiased text-gray-800">
+        <Header />
+        <main className="mt-6 mb-10">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
   )
 }
 
--- pages/posts/[...slug].js
+++ pages/posts/[...slug].js
@@ -3,8 +3,12 @@
 import { getMdxNode, getMdxPaths } from 'next-mdx/server'
 import { useHydrate } from 'next-mdx/client'
 import { mdxComponents } from '../../components/mdx-components'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export default function PostPage({ post }) {
+  //hook
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
+
   //Yazı içine component yerleştirmek için bu paketi kullanıyoruz.
   const content = useHydrate(post, {
     components: mdxComponents
@@ -12,12 +16,49 @@ export default function PostPage({ post }) {
 
   return (
     <div className="site-container">
-      <article className="space-y-4">
+      <article className="space-y-4 mb-6">
         <h1 className="text-4xl">{post.frontMatter.title}</h1>
         <p>{post.frontMatter.excerpt}</p>
         <hr />
         <div className="prose">{content}</div>
       </article>
+
+      <form className="mt-10">
+        <textarea
+          rows="3"
+          placeholder="Leave a comment"
+          className="w-full block border mb-4 resize-y px-2 py-1 bg-gray-100 placeholder-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+        />
+        {isAuthenticated ? (
+          <div className="flex items-center space-x-4">
+            <button className="buttons">Send</button>
+            <img
+              src={user.picture}
+              width={30}
+              className="rounded-full"
+              alt="profile"
+            />
+            <span className="">{user.name}</span>
+            <button
+              type="button"
+              className=""
+              onClick={() =>
+                logout({ returnTo: process.env.NEXT_PUBLIC_URL + '/posts' })
+              }
+            >
+              x
+            </button>
+          </div>
+        ) : (
+          <button
+            type="button"
+            className="buttons"
+            onClick={() => loginWithRedirect()}
+          >
+            Login
+          </button>
+        )}
+      </form>
     </div>
   )
 }
